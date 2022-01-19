a0c96ff804292fe1732f60b60a01492a31fe3b64
Rachel Purganan <rpurganan.inc@gmail.com>
2022-01-19 11:30:52 -0800

    adds if statement to make form disappear once there is data in the form

--- js/data-processing.js
+++ js/data-processing.js
@@ -1,26 +1,36 @@
 //use location object to access querystring (address bar)
 const queryString = window.location.search;
+let myData = '';//will store data for output
+
+if(queryString!=""){//process data
+  
+    //separate querystring parameters
+    const urlParams = new URLSearchParams(queryString);
+
+
+    urlParams.forEach(function(value, key) {
+        
+        //https://stackoverflow.com/questions/542232/in-javascript-how-can-i-perform-a-global-replace-on-string-with-a-variable-insi
+
+        //this will replace underscore with spaces
+      key = key.split("_").join(" ");
+
+            //replaces underscore with space ONLY ONCE. Example: Join Mailing_List
+            //key = key.replace("_"," ");
+        
+      myData += `<p>${key}: ${value}</p>`;
+      
+      //console.log(value, key);
+    });
+
+    //creates CLEAR button to clear the data
+    myData += `<p><a href="index.html">CLEAR</a></p>`
+    //pushes everything to that output div
+    document.getElementById("output").innerHTML = myData;
+
+
+};
     
 //output to console    
 //console.log(queryString);
     
-//separate querystring parameters
-const urlParams = new URLSearchParams(queryString);
-
-let myData = '';
-urlParams.forEach(function(value, key) {
-    
-    //https://stackoverflow.com/questions/542232/in-javascript-how-can-i-perform-a-global-replace-on-string-with-a-variable-insi
-
-    //this will replace underscore with spaces
-   key = key.split("_").join(" ");
-    
-   myData += `<p>${key}: ${value}</p>`;
-   
-   //console.log(value, key);
- });
-
-
-myData += `<p><a href="index.html">CLEAR</a></p>`
- //pushes everything to that output div
- document.getElementById("output").innerHTML = myData;
